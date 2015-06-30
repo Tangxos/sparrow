55a89cf8f1928c42717c28999ec06b44dd076de1
wufuxi <justin_wu78@yahoo.com>
2015-06-30 22:35:57 +0800

    package.json and gulpfile.js commit at 6.30

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    less = require('gulp-less'),
+    minifycss = require('gulp-minify-css'),
+    xinclude = require('gulp-xinclude');
+
